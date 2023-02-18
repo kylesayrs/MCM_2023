53912236a64ad4ff4b14b76e3aeb9556e83394c2
Kyle Sayers <kylesayrs@gmail.com>
2023-02-17 20:41:26 -0500

    add make_plant_population_variables

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import numpy
 
 from config import BaseConfig
 from simulation import Simulation
-from smc import StochasticMarkovChain
-from population_variable import PopulationVariable
-from visualize import plot_values, plot_population_time, plot_population, show_plot
+from population_variable import make_plant_population_variables
+from visualize import plot_population_time, plot_population, show_plot
 
 
 if __name__ == "__main__":
@@ -14,21 +13,10 @@ if __name__ == "__main__":
     numpy.random.seed(config.seed)
 
     # create population variables
-    populations = {
-        "plant_x": PopulationVariable(f"plant_x", 1.0),
-        "plant_y": PopulationVariable(f"plant_y", 1.0)
-    }
-
-    populations["plant_x"].ddt = (
-        lambda:
-        4.0 * populations["plant_x"].value +
-        (numpy.array([0, -2.0]) @ numpy.array([var.value for var in populations.values()])) * populations["plant_x"].value
-    )
-    populations["plant_y"].ddt = (
-        lambda:
-        -3.0 * populations["plant_y"].value +
-        (numpy.array([1.0, 0]) @ numpy.array([var.value for var in populations.values()])) * populations["plant_y"].value
-    )
+    initial = numpy.array([1.0, 1.0])
+    growth = numpy.array([4.0, -3.0])
+    interactions = numpy.array([[0.0, -2.0], [1.0, 0.0]])
+    populations = make_plant_population_variables(2, initial, growth, interactions)
 
     # create simulation
     simulation = Simulation(
@@ -48,7 +36,7 @@ if __name__ == "__main__":
     show_plot()
     plot_population(
         simulation.population_history,
-        "plant_x", "plant_y",
-        reduce_factor=1000
+        "plant_0", "plant_1",
+        reduce_factor=10
     )
     show_plot()
--- population_variable.py
+++ population_variable.py
@@ -9,23 +9,29 @@ def make_plant_population_variables(
     growth: numpy.ndarray,
     interactions: numpy.ndarray,
 ):
-    population_variables = {
+    # initialize variables
+    variables = {
         f"plant_{plant_i}": PopulationVariable(f"plant_{plant_i}", initial_i)
         for plant_i, initial_i in zip(range(num_plants), initial)
     }
 
-    for variable_i, variable in enumerate(population_variables.values()):
+    # deep copy arguments for lambda closure
+    _growth = growth.copy()
+    _interactions = interactions.copy()
+
+    # define derivative equations (variables is mutable)
+    for variable_i, variable in enumerate(variables.values()):
         variable.ddt = (
-            lambda:
-            growth[variable_i] * variable.value +
+            lambda _variable_i=variable_i, _variable=variable:
+            _growth[_variable_i] * _variable.value +
             (
-                interactions[variable_i] @
-                numpy.array([var.value for var in population_variables.values()]) *
-                variable.value
+                _interactions[_variable_i] @
+                numpy.array([var.value for var in variables.values()]) *
+                _variable.value
             )
         )
 
-    return population_variables
+    return variables
 
 
 class PopulationVariable:
@@ -44,7 +50,7 @@ class PopulationVariable:
 
     def step(self, h: Optional[float] = 1.0):
         if self.ddt is None:
-            raise ValueError("ddt is not defined!")
+            raise ValueError(f"ddt is not defined for {self.name}")
 
         self.new_value = max(self.value + (h * self.ddt()), 0.0)
         return self.new_value
@@ -54,3 +60,6 @@ class PopulationVariable:
         self.value = self.new_value
         self.new_value = None
 
+
+    def __str__(self):
+        return f"PopulationVariable(name={self.name}, value={self.value})"
